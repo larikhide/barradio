eb92bfafc83e6c33c3769d5ca7da29f2b7b765a3
shda <shda@pop-os.localdomain>
2022-02-13 01:49:13 +0300

    added migration files

---
+++ migrations/000003_vote_table.up.sql
@@ -0,0 +1,22 @@
+
+
+-- DROP TABLE IF EXISTS vote;
+
+CREATE TABLE IF NOT EXISTS vote
+(
+    id uuid NOT NULL,
+    category_id uuid NOT NULL,
+    category_code integer NOT NULL,
+    date timestamp without time zone NOT NULL,
+    CONSTRAINT vote_pkey PRIMARY KEY (id)
+);
+
+
+
+COMMENT ON TABLE vote IS 'голоса';
+COMMENT ON COLUMN vote.id  IS 'идентификатор';
+COMMENT ON COLUMN vote.category_id IS 'иднгтификатор категории (uuid)';
+COMMENT ON COLUMN vote.category_code IS 'код категории (int)';
+COMMENT ON COLUMN vote.date  IS 'дата время голосования';
+
+
